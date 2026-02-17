33d746cd5fc5105f0d169d80941d10f9d2893bfb
cryptokiller0 <tenutaachille@gmail.com>
2026-02-17 10:04:41 +0100

    first upload

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const TelegramBot = require("node-telegram-bot-api");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const token = "8206549222:AAFggPjLfWq2OJKSQTafTnlo1XntUOnHtFM";
+const bot = new TelegramBot(token, { polling: true });
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => {
+  console.log("Server attivo su http://localhost:3000");
+});
+
+bot.onText(/\/start/, (msg) => {
+  bot.sendMessage(msg.chat.id, "Apri il catalogo:", {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "Apri Catalogo",
+            web_app: { url: "http://localhost:3000" }
+          }
+        ]
+      ]
+    }
+  });
+});
+
